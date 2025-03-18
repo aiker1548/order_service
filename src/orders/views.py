641ce1aa8cccef6eb46e046eec7c9335e1fd5f2d
aiker1548 <aiker03@mail.ru>
2025-03-18 16:10:41 +0300

    feat: add route taken orders

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import time
 
 from fastapi import APIRouter, Depends, HTTPException
 
@@ -7,6 +8,9 @@ from src.database.core import DbDependency
 from src.orders.service import create_order, verify_user, take_order as take_order_service
 from src.orders.service import get_my_taken_orders
 from src.orders.schemas import OrderWithUserDetails
+from src.kafka.producer import send_user_ids_to_kafka
+from src.kafka.consumer import consume_kafka_messages_for_users
+from src.orders.schemas import User
 
 router = APIRouter(prefix="/orders", tags=["orders"])
 
@@ -30,18 +34,37 @@ async def take_order(
         raise HTTPException(status_code=404, detail="Order not found")
     return order
     
-
+    
 @router.get("/taken/", response_model=list[OrderWithUserDetails])
 async def my_orders(
     db: DbDependency,
     payload=Depends(verify_user)
 ):
     orders = await get_my_taken_orders(db, payload['user_id'])
-    user_ids = set()
-    user_ids.add(order['executor_id'])
+    user_ids = {order.creator_id for order in orders} | {payload['user_id']}
+    
+    start = time.time()
+    correlation_id = await send_user_ids_to_kafka(list(user_ids))
+    users_data = await consume_kafka_messages_for_users(correlation_id, user_ids)
+    print(f"Kafka time: {time.time() - start:.3f} sec")
+    
+    orders_with_users = []
     for order in orders:
-        user_ids.add(order['executor_id'])
-    
-    return orders
-    
+        creator_user = users_data.get(order.creator_id)
+        executor_user = users_data.get(order.executor_id)
+        orders_with_users.append(OrderWithUserDetails(
+            id=order.id,
+            creator=User(username=creator_user['username']) if creator_user else None,
+            executor=User(username=executor_user['username']) if executor_user else None,
+            title=order.title,
+            price=order.price,
+            details=order.details,
+            status=order.status,
+            created_at=order.created_at,
+            updated_at=order.updated_at,
+            executor_id=order.executor_id,
+            creator_id=order.creator_id
+        ))
+
+    return orders_with_users
 
